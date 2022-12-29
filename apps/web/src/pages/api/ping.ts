37986756551ad8c3038ee6ac7fba4d7dceda901f
3mil0 <dude@stokedbits.com>
2022-12-29 09:16:19 -0600

    adds working project with updated readme

---
+++ apps/web/src/pages/api/ping.ts
@@ -0,0 +1,52 @@
+import {connect} from "amqplib"
+import type { NextApiRequest, NextApiResponse } from "next";
+import logger from "logger";
+
+/**
+ * Users
+ *
+ * @description A basic API endpoint to retrieve all the users in the database
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res.setHeader("Allow", ["GET"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  try {
+    // Get the queue url
+    const QUEUE_URL = process.env.QUEUE_URL
+
+    // blow up if there is no queue url since it is required
+    if (!QUEUE_URL) {
+        throw new Error("QUEUE_URL is not defined")
+    }
+
+    // establish a connection to the queue
+    const connection = await connect(QUEUE_URL);
+
+    // open a channel on the queue
+    const queueChannel = await connection.createChannel();
+
+    // assert there is a queue, create and make sure it's configured to our specifications
+    await queueChannel.assertQueue('worker-ping')
+
+    for (let i = 0; i < 1000; i++) {
+      queueChannel.sendToQueue('worker-ping', Buffer.from(JSON.stringify({ping: 'Hey there 👋'})))
+  
+      // hang around for the message to be sent to the queue
+      setTimeout(async () => {
+      }, 500)
+    }
+
+    logger.info('All good!')
+    return res.status(200).end()
+  } catch (e) {
+    logger.error(e)
+
+    return res.status(500).end('Something blew up!');
+  }
+}
